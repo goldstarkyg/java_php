ac820b4e78041434f9dfc1f02abf79bb85387770
goldstarkyg <goldstarkyg91@gmail.com>
2017-09-23 10:03:30 +0800

    update code

--- resources/views/layouts/app.blade.php
+++
@@ -1,9 +0,0 @@
-@include('includes.header')
-        <!---Header Section End Here-->
-<div id="content">
-    @yield('content')
-</div>
-<!---work Section End Here-->
-<div class="clearfix"></div>
-<!---Footer Section Start Here-->
-@include('includes.footer')
